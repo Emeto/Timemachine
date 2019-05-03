9ea81599ed2a1436ad653425d184d8c77fee7a62
Jérémy Castellano <jeremy.castellano@me.com>
2019-05-03 19:48:45 +0200

    Removed references to Tool.vue

--- resources/js/tool.js
+++ resources/js/tool.js
@@ -1,12 +1,4 @@
 Nova.booting((Vue, router, store) => {
-    router.addRoutes([
-        {
-            name: 'timemachine',
-            path: '/timemachine',
-            component: require('./components/Tool'),
-        },
-    ]);
-
     Vue.component('custom-detail-toolbar', require('./components/CustomDetailToolbar'));
     Vue.component('users-detail-toolbar', require('./components/UsersDetailToolbar'));
 });
